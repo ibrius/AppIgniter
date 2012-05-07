a6e7a5b49690080175b2ab49203254d85320b07f
Team Ibrius <admin@ibrius.net>
2012-05-07 00:06:00 -0700

    Changed the relative paths to the css files so that they will still work if the app is in another folder above the web root

--- application/views/tab/partial/headercanvas.php
+++ application/views/tab/partial/headercanvas.php
@@ -3,7 +3,7 @@
 <head>
 	<title><?php echo $this->lang->line('common_my_app');?></title>	
 	<meta charset="utf-8">   
- 	<link rel="stylesheet" href="/public/css/stylecanvas.css" type="text/css" media="all">
+ 	<link rel="stylesheet" href="../css/stylecanvas.css" type="text/css" media="all">
 	<meta property="og:description" content="<?php echo $this->lang->line('common_app_description');?>" >
 </head>
 <body>
