eda83b3c43b7ccb2f09c185512dabd936a5e9b8e
Team Ibrius <admin@ibrius.net>
2012-05-09 01:48:12 -0700

    Changed the name in the license notification

--- application/models/create.php
+++ application/models/create.php
@@ -1,21 +1,21 @@
 <?php
 /**
-Copyright 2012 Marina Ibrishimova and Byron Matus
+Copyright 2012 Marina Ibrishimova and Byron Matus | Contact: admin@ibrius.net
 
-This file is part of FB Frapp.
+This file is part of AppIgniter.
 
-    FB Frapp is free software: you can redistribute it and/or modify
+    AppIgniter is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    FB Frapp is distributed in the hope that it will be useful,
+    AppIgniter is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with FB Frapp.  If not, see <http://www.gnu.org/licenses/>.
+    along with AppIgniter.  If not, see <http://www.gnu.org/licenses/>.
 **/
 class Create extends CI_Model 
 {
